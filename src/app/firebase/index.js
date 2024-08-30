08f79b146158c0200f432f07fdfffd9cfde35bdc
Gabriel Wanderley <gabrielwanderley08@gmail.com>
2024-08-30 10:17:55 -0300

    last

--- src/app/firebase/index.js
+++ src/app/firebase/index.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage, ref } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHsw1VYAX5ZyH0Jrbd6njDbcxuoH2Nv_w",
-  authDomain: "talent-884db.firebaseapp.com",
-  projectId: "talent-884db",
-  storageBucket: "talent-884db.appspot.com",
-  messagingSenderId: "568498800529",
-  appId: "1:568498800529:web:b08741eddb5b4cdfff7c30"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
